ab55bac8111a43ec3449831218251f913a3c620c
HMB <johndoe@example.com>
2023-02-24 21:03:48 +0000

    fix encoding for JSON

--- hiring_23_tech.py
+++ hiring_23_tech.py
@@ -4,7 +4,7 @@ from pprint import pprint
 
 
 # Open the JSON file
-with open("hiring.json") as file:
+with open("hiring.json", encoding = "utf8") as file:
     # Load the JSON data from the file
     hiring_data = json.load(file)
 
